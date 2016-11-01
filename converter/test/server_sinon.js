4a85816e904a1042feb9e5566a7e54fbaef7920d
Art Vendelaine <arty31@users.noreply.github.com>
2016-11-01 18:52:15 +0100

    initial working version.

---
+++ converter/test/server_sinon.js
@@ -0,0 +1,60 @@
+var expect  = require("chai").expect;
+var request = require("request");
+var sinon = require("sinon");
+
+describe("Sinon Tests Color Code Converter API", function() {
+    
+    //Create a spy for the Request function
+    var requestSpy = sinon.spy(request, 'Request');
+
+  describe("RGB to Hex conversion", function() {
+
+    var url = "http://localhost:3000/rgbToHex?red=255&green=255&blue=255";
+
+    it("returns status 200", function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+        printSpyInfo();
+      });
+    });
+
+    it("returns the color in hex", function(done) {
+      request(url, function(error, response, body) {
+        expect(body).to.equal("ffffff");
+        done();
+        printSpyInfo();
+      });
+    });
+
+  });
+
+  describe("Hex to RGB conversion", function() {
+    var url = "http://localhost:3000/hexToRgb?hex=00ff00";
+
+    it("returns status 200", function(done) {
+      request(url, function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+        printSpyInfo();
+      });
+    });
+
+    it("returns the color in RGB", function(done) {
+      request(url, function(error, response, body) {
+        expect(body).to.equal("[0,255,0]");
+        done();
+        printSpyInfo();
+      });
+    });
+  });
+  
+  
+  function printSpyInfo(){
+    console.log('requestSpy.callCount:'+ requestSpy.callCount); 
+    console.log('requestSpy.returned:'+ requestSpy.returned());
+    requestSpy.reset();
+  }
+  
+
+});
