bf78f5a35d90ae8d083e46b0d5f84144606b1f9d
Art Vendelaine <arty31@users.noreply.github.com>
2016-12-04 12:30:03 +0100

    added karma config for travis.

--- converter/karma.conf.js
+++ converter/karma.conf.js
@@ -92,7 +92,7 @@ transform: [
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
